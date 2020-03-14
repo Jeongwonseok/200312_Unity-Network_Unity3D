d6a0d6d190f9ab66ac62d48cab8efb6fc2f431fa
“Jeongwonseok” <jws4281@nate.com>
2020-03-14 19:02:49 +0900

    Health Identity
    - 체력 동기화

--- UNet Shooter/Assets/Script/Bullet.cs
+++ UNet Shooter/Assets/Script/Bullet.cs
@@ -7,6 +7,13 @@ public class Bullet : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {
         // 충돌한 상대방에게 데미지를 주는 코드
+        var hit = collision.gameObject;
+        var health = hit.GetComponent<Health>();
+
+        if(health != null)
+        {
+            health.TakeDamage(10);
+        }
 
         Destroy(gameObject);
     }
---
+++ UNet Shooter/Assets/Script/Health.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class Health : NetworkBehaviour
+{
+    public const int maxHealth = 100;
+
+    // 자동으로 값을 클라이언트에게도 변경해주는 태그  & 훅 걸기
+    [SyncVar(hook = "OnChangeHealth")]
+    public int currentHealth = maxHealth;
+
+    public Slider healthSlider;
+
+    // 데미지 손실 함수
+    public void TakeDamage(int amount)
+    {
+        if(!isServer)
+        {
+            return;
+        }
+        // 데미지 계산 코드
+        currentHealth -= amount;
+    }
+
+    void OnChangeHealth(int health)
+    {
+        healthSlider.value = health;
+    }
+}
