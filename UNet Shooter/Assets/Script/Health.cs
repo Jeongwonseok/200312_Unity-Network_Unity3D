4cc4c32d020f0c8da740f3fa383e27f28262282b
“Jeongwonseok” <jws4281@nate.com>
2020-03-16 16:29:18 +0900

    Respawn
    - 리스폰 구현
    - ClienRpc 태그 사용

--- UNet Shooter/Assets/Script/Health.cs
+++ UNet Shooter/Assets/Script/Health.cs
@@ -14,6 +14,18 @@ public class Health : NetworkBehaviour
 
     public Slider healthSlider;
 
+    // NetworkStartPosition 배열 선언
+    private NetworkStartPosition[] spawnPoints;
+
+    void Start()
+    {
+        // LocalPlayer 이면 >> 모든 StartPoint를 대입해준다. 
+        if(isLocalPlayer)
+        {
+            spawnPoints = FindObjectsOfType<NetworkStartPosition>();
+        }
+    }
+
     // 데미지 손실 함수
     public void TakeDamage(int amount)
     {
@@ -23,10 +35,37 @@ public class Health : NetworkBehaviour
         }
         // 데미지 계산 코드
         currentHealth -= amount;
+
+        if(currentHealth <= 0)
+        {
+            currentHealth = maxHealth;
+
+            // Rpc : 서버에서 발동하면 >> 모든 클라이언트들에서도 자동으로 발동됨
+            RpcRespawn();
+        }
     }
 
     void OnChangeHealth(int health)
     {
         healthSlider.value = health;
+    }
+
+    // ClientRpc 태그 : 서버에서 발동하면 >> 모든 클라이언트들에서도 자동으로 발동됨
+    [ClientRpc]
+    void RpcRespawn()
+    {
+        if(isLocalPlayer)
+        {
+            Vector3 spawnPoint = Vector3.zero;
+
+            // 리스폰포인트 배열의 값이 존재하고, 길이가 0보다 크면 >> 랜덤으로 리스폰 포인트를 지정해주기
+            if(spawnPoints != null && spawnPoints.Length > 0)
+            {
+                spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)].transform.position;
+            }
+
+            // spawnPoint 위치를 지정
+            transform.position = spawnPoint;
+        }
     }
 }
